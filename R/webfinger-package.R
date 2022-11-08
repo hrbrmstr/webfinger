51b8a887d7a21ff3dbe2d2b7f0ce886553d0ae00
boB Rudis <bob@rud.is>
2022-11-08 08:39:43 -0500

    R package repo initialization complete

---
+++ R/webfinger-package.R
@@ -0,0 +1,10 @@
+#' ...
+#' 
+#' @md
+#' @name webfinger
+#' @keywords internal
+#' @author Bob Rudis (bob@@rud.is)
+#' @import httr
+#' @importFrom jsonlite fromJSON
+# @ useDynLib webfinger, .registration = TRUE
+"_PACKAGE"
